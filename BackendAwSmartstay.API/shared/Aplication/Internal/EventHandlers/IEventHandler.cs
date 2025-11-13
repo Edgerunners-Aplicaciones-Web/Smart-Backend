74433967dd5b8efc61eb10ee6718a2703cf96564
shiloox <ada44lovelace@gmail.com>
2025-11-12 19:39:22 -0500

    feat(shared): add event handler interface

---
+++ BackendAwSmartstay.API/shared/Aplication/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using BackendAwSmartstay.API.Shared.Domain.Model.Events;
+using Cortex.Mediator.Notifications;
+
+namespace BackendAwSmartstay.API.shared.Aplication.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+    
+}
