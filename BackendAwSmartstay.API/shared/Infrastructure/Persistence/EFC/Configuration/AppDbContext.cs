8625072fe4f0714b92b546942f31ae23766ced22
shiloox <u202019498@upc.edu.pe>
2025-11-13 16:20:29 -0500

    feat(accommodations): implement Accommodation repository and configuration for entity mappings

--- BackendAwSmartstay.API/shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ BackendAwSmartstay.API/shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+﻿using BackendAwSmartstay.API.Accommodations.Domain.Entities;
+using BackendAwSmartstay.API.Accommodations.Infrastructure.Persistence.EFC.Configuration;
+using BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +8,14 @@ namespace BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configura
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-
+    // Accommodations Context
+    public DbSet<Accommodation> Accommodations => Set<Accommodation>();
+    public DbSet<AccommodationType> AccommodationTypes => Set<AccommodationType>();
+    public DbSet<AccommodationSubType> AccommodationSubTypes => Set<AccommodationSubType>();
+    public DbSet<Amenity> Amenities => Set<Amenity>();
+    public DbSet<AccommodationAmenity> AccommodationAmenities => Set<AccommodationAmenity>();
+    public DbSet<Room> Rooms => Set<Room>();
+    
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.AddCreatedUpdatedInterceptor();
@@ -17,15 +26,14 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
 
-        // Hotel Context
-        //builder.ApplyPublishingConfiguration();
-
-        // Profiles Context
-        //builder.ApplyProfilesConfiguration();
+        // Accommodations Context Configuration
+        builder.ApplyConfiguration(new AccommodationsConfiguration());
+        builder.ApplyConfiguration(new AccommodationTypeConfiguration());
+        builder.ApplyConfiguration(new AccommodationSubTypeConfiguration());
+        builder.ApplyConfiguration(new AmenityConfiguration());
+        builder.ApplyConfiguration(new AccommodationAmenityConfiguration());
+        builder.ApplyConfiguration(new RoomConfiguration());
         
-        // IAM Context
-        // TODO: Apply IAM configuration when available
-        // builder.ApplyIAMConfiguration();
         
         // General Naming Convention for the database objects
         builder.UseSnakeCaseNamingConvention();
