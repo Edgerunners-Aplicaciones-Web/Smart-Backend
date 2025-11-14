45b5086c75369959cfbb5136a805118988d26f19
shiloox <ada44lovelace@gmail.com>
2025-11-14 15:26:58 -0500

    refactor(payments): add payment command/query services and payment repository interfaces

--- BackendAwSmartstay.API/Payments/Domain/Repositories/IPaymentRepository.cs
+++ BackendAwSmartstay.API/Payments/Domain/Repositories/IPaymentRepository.cs
@@ -3,9 +3,6 @@ using BackendAwSmartstay.API.Shared.Domain.Repositories;
 
 namespace BackendAwSmartstay.API.Payments.Domain.Repositories;
 
-/// <summary>
-///     Represents the payment repository in the hotel management system.
-/// </summary>
 public interface IPaymentRepository : IBaseRepository<Payment>
 {
 }
